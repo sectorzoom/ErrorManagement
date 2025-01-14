8b7b6b34adf36ec715f67e4370cb7acb0b39cc99
David Gimenez <sectorzoom@gmail.com>
2025-01-14 12:50:05 +0100

    Initial commit

---
+++ src/main/java/org/example/errormanagement/ErrorManagementApplication.java
@@ -0,0 +1,13 @@
+package org.example.errormanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/errormanagement/ErrorManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.errormanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
